bb227e9b809f13242d0af6cd11965454f1602d80
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2022-06-06 17:10:55 -0500

    Remove extra days and hours

--- backend/venues/views.py
+++ backend/venues/views.py
@@ -67,7 +67,7 @@ class SpecialsView(APIView):
 		j = 0
 		while i < len(barnames):
 			try:
-				events[i] = {"venue": {"title": barnames[i], "address": contacts[i]}, "drink_deals": {"days": specials[j][0], "hours": specials[j][1], "info": specials[j]}, "food_deals": {"days": specials[j+1][0], "hours": specials[j+1][1], "info": specials[j+1]}}
+				events[i] = {"venue": {"title": barnames[i], "address": contacts[i]}, "drink_deals": {"days": specials[j][0], "hours": specials[j][1], "info": specials[j][2:]}, "food_deals": {"days": specials[j+1][0], "hours": specials[j+1][1], "info": specials[j+1][2:]}}
 			except:
 				events[i] = {"venue": {"title": barnames[i], "address": contacts[i]}, "drink_deals": {"days": specials[j], "hours": specials[j], "info": specials[j]}, "food_deals": {"days": specials[j+1], "hours": specials[j+1], "info": specials[j+1]}}
 			i += 1
